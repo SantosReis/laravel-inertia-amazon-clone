9f47be9810a35c9efba67e8a028ac27b59c204b5
Marcus Reis <mvsantosreis@gmail.com>
2024-05-24 10:59:00 +0100

    Product Section Implementation

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CategoryController;
 
@@ -30,6 +31,7 @@ Route::get('/', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
 Route::get('/category/{id}', [CategoryController::class, 'index'])->name('category.index');
+Route::get('/product/{id}', [ProductController::class, 'index'])->name('product.index');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
